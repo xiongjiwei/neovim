d3b24c5aa9c1f03567280f8cfeaaefd62cd676a1
xiongjiwei <xiongjiwei1996@outlook.com>
2023-06-27 14:23:48 +0800

    add more colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,13 +2,20 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "melange",
+			colorscheme = "gruvbox-material",
 		},
 	},
-    {
-        "rebelot/kanagawa.nvim",
-    },
-    {
-        "savq/melange-nvim",
-    },
+	{
+		"rebelot/kanagawa.nvim",
+	},
+	{
+		"savq/melange-nvim",
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "doums/darcula" },
+	{ "sainnhe/gruvbox-material" },
+	{ "sainnhe/edge" },
+	{ "olimorris/onedarkpro.nvim" },
+	{ "morhetz/gruvbox" },
+	{ "navarasu/onedark.nvim" },
 }
