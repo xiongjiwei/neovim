b06e66f378e276fb2fc0eff9f98b49e22e98e07a
xiongjiwei <xiongjiwei1996@outlook.com>
2023-06-28 14:50:56 +0800

    add overseer

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -39,6 +39,7 @@ return {
 			}
 			opts.inactive_winbar = {
 				lualine_a = {
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
 					{ "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
 				},
 				lualine_z = {
